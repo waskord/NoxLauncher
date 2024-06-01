3aa79f4c5b44b888ee8b1b7e1abc4819fa001cce
Waskord <waskord3@gmail.com>
2024-06-01 21:05:30 +0300

    update

--- app/assets/js/scripts/uibinder.js
+++ app/assets/js/scripts/uibinder.js
@@ -80,10 +80,7 @@ async function showMainUI(data){
             validateSelectedAccount()
         }
 
-        if(ConfigManager.isFirstLaunch()){
-            currentView = VIEWS.welcome
-            $(VIEWS.welcome).fadeIn(1000)
-        } else {
+
             if(isLoggedIn){
                 currentView = VIEWS.landing
                 $(VIEWS.landing).fadeIn(1000)
@@ -94,15 +91,15 @@ async function showMainUI(data){
                 currentView = VIEWS.loginOptions
                 $(VIEWS.loginOptions).fadeIn(1000)
             }
-        }
+
 
         setTimeout(() => {
             $('#loadingContainer').fadeOut(500, () => {
                 $('#loadSpinnerImage').removeClass('rotating')
             })
-        }, 250)
+        }, 0)
         
-    }, 750)
+    }, 0)
     // Disable tabbing to the news container.
     initNews().then(() => {
         $('#newsContainer *').attr('tabindex', '-1')
